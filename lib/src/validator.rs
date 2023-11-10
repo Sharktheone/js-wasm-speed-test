5757e139ac0cf5599d0912a6de8c31b5524ba501
Sharktheone <sharktheone@proton.me>
2023-11-10 19:10:05 +0100

    first http validator, but benchmark needs some work

--- lib/src/validator.rs
+++ lib/src/validator.rs
@@ -1,6 +1,10 @@
 use std::str::FromStr;
-use http::method::Method;
-use http::request::Request;
+use std::sync::{Arc, mpsc, Mutex};
+use std::time::Duration;
+
+use reqwest::{Method, RequestBuilder, StatusCode};
+use reqwest::blocking::RequestBuilder as BlockingRequestBuilder;
+use reqwest::header::{HeaderMap, HeaderName};
 
 /// # Validator
 /// Validate results
@@ -9,6 +13,8 @@ use http::request::Request;
 /// - Validate by http response
 /// - Validate by http response code
 
+
+const BENCHMARK_THREADS: u8 = 16;
 
 
 pub struct Validator {
@@ -58,9 +64,16 @@ pub struct ConsoleResult<'a> {
 
 pub struct HTTPResult<'a> {
     pub http: &'a HTTP,
-    pub result: bool,
+    pub result: HTTPResultType,
     pub response: String,
     pub response_code: u16,
+}
+
+pub enum HTTPResultType {
+    Success,
+    Partial,
+    Always,
+    Fail,
 }
 
 impl Validator {
@@ -126,16 +139,16 @@ impl Validator {
     }
 
     pub fn validate_console(&self, out: String) -> ConsoleResult {
-        let real= out.lines();
+        let real = out.lines();
         let expected = self.console.iter();
 
         let res_individually = real.zip(expected).map(|(r, e)| r == e).collect::<Vec<bool>>();
         let res_individually = res_individually.to_owned();
 
 
-            let real: Vec<String> = out.lines().map(|s| s.to_string()).collect();
-            let expected = &self.console;
-            let result = &real == expected;
+        let real: Vec<String> = out.lines().map(|s| s.to_string()).collect();
+        let expected = &self.console;
+        let result = &real == expected;
 
         ConsoleResult {
             console: &self.console,
@@ -147,14 +160,9 @@ impl Validator {
 
 
     pub fn validate_http(&self) -> Vec<HTTPResult> {
-
         let mut results = vec![];
 
         for http in &self.http {
-
-
-
-
             let method = match http.method {
                 HTTPMethod::GET => Method::GET,
                 HTTPMethod::POST => Method::POST,
@@ -163,44 +171,125 @@ impl Validator {
                 HTTPMethod::PATCH => Method::PATCH,
             };
 
-            let request = Request::builder()
-                .uri(&http.url)
-                .method(method);
 
+            let mut headers = HeaderMap::new();
             for header in &http.headers {
                 let mut split = header.split(":");
-                let key = split.next().unwrap();
-                let value = split.next().unwrap();
+                // let key = split.next().unwrap().to_owned().parse().unwrap();
+                let key = HeaderName::from_str(split.next().unwrap()).unwrap();
+                let value = split.next().to_owned().unwrap().parse().unwrap();
 
-                request.header(key, value);
+                headers.insert(key, value);
             }
+            if http.benchmark {
+                let client = reqwest::Client::new();
 
-            //TODO hmm, the request crate is not what I thought it was...
+                let request = client
+                    .request(method, &http.url)
+                    .headers(headers)
+                    .body(http.payload.clone());
 
-            let response;
+
+                let res = benchmark(request, BENCHMARK_THREADS, Duration::from_secs(5));
+
+                for (success, code, text) in res {
+                    results.push(HTTPResult {
+                        http,
+                        result: if success { HTTPResultType::Success } else { HTTPResultType::Fail },
+                        response_code: code.as_u16(),
+                        response: text,
+                    });
+                }
+            } else {
+                let client = reqwest::blocking::Client::new();
 
 
+                let request = client
+                    .request(method, &http.url)
+                    .headers(headers)
+                    .body(http.payload.clone());
 
-            let result = response == http.response && response_code == http.response_code;
 
-            results.push(HTTPResult {
-                http,
-                result,
-                response,
-                response_code,
-            });
+                let res = check(request, http.response.clone(), http.response_code);
+
+                results.push(HTTPResult {
+                    http,
+                    result: if res.0 { HTTPResultType::Success } else { HTTPResultType::Fail },
+                    response_code: res.1.as_u16(),
+                    response: res.2,
+                });
+            }
         }
 
         results
     }
-
-
-
-
 }
 
 impl Default for Validator {
     fn default() -> Self {
         Self::new()
     }
+}
+
+fn benchmark(request: RequestBuilder, threads: u8, duration: Duration) -> Vec<(bool, StatusCode, String)> {
+    let mut handles = vec![];
+    let finished = Arc::new(Mutex::new(false));
+    let request = Arc::new(request);
+    let mut res = vec![];
+
+    let (tx, rx) = mpsc::channel();
+    let tx = Arc::new(tx);
+
+    for _ in 0..threads {
+        let finished = Arc::clone(&finished);
+        let request = Arc::clone(&request);
+        let tx = Arc::clone(&tx);
+        let handle = std::thread::spawn(|| async move {
+            let mut status = vec![];
+            while !*finished.lock().unwrap() {
+                let res = request.try_clone().unwrap().send();
+                status.push(res);
+            }
+
+            let mut res = Vec::with_capacity(status.len());
+
+            for status in status {
+                let status = status.await.unwrap(); //TODO this is dumb... we don't need to await somewhere else, so it does the work earlier - tokio?
+                let code = status.status();
+                let text = status.text().await.unwrap();
+
+                res.push((false, code, text));
+            }
+
+            tx.send(res).unwrap();
+        });
+
+        handles.push(handle);
+    }
+
+    std::thread::sleep(duration);
+
+    let mut finished = finished.lock().unwrap();
+    *finished = true;
+
+    for _ in handles {
+        res.append(&mut rx.recv().unwrap());
+    }
+
+    res
+}
+
+fn check(request: BlockingRequestBuilder, response: String, response_code: u16) -> (bool, StatusCode, String) {
+    let handle = std::thread::spawn(|| {
+        let res = request.send().unwrap();
+
+        let code = res.status();
+        let text = res.text().unwrap();
+
+        (code, text)
+    });
+
+    let (code, text) = handle.join().unwrap();
+
+    (code == response_code && text == response, code, text)
 }
