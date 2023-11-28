561e499441b54e88bf7d7da9ed9c0657bec09cec
Sharktheone <sharktheone@proton.me>
2023-11-28 16:22:10 +0100

    i need to rewrite this without mozjs_sys later...

--- lib/src/js/spidermonkey.rs
+++ lib/src/js/spidermonkey.rs
@@ -1,14 +1,17 @@
 use ::std::path::Path;
 use ::std::ptr;
-use deno_core::serde_v8::Serializable;
-use javascriptcore::ContextExt;
-use sysinfo::ProcessStatus::Run;
 
-use crate::errors::TestError;
-use crate::js::runner::run;
-use crate::js::{JSEngine, JSRunner};
-use crate::validator::Validator;
+use mozjs::jsapi::*;
+use mozjs::jsval::UndefinedValue;
+use mozjs::rooted;
+use mozjs::rust::{RealmOptions, Runtime};
+use mozjs::rust::SIMPLE_GLOBAL_CLASS;
+
 use crate::{Engine, TestResult};
+use crate::errors::TestError;
+use crate::js::{JSEngine, JSRunner};
+use crate::js::runner::run;
+use crate::validator::Validator;
 
 pub struct SpiderMonkey;
 
@@ -35,23 +38,48 @@ impl JSRunner for SpiderMonkey {
             validator,
             Engine::JS(JSEngine::SpiderMonkey),
             |(file, reruns)| {
-                use mozjs::jsapi::*;
-                use mozjs::jsval::UndefinedValue;
-                use mozjs::rooted;
-                use mozjs::rust::SIMPLE_GLOBAL_CLASS;
-                use mozjs::rust::{RealmOptions, Runtime};
-
                 let engine = mozjs::rust::JSEngine::init().unwrap();
                 let rt = Runtime::new(engine.handle());
 
                 let options = RealmOptions::default();
-                rooted!(in(rt.cx()) let global = unsafe {
+                rooted!(in(rt.cx()) let global  = unsafe {
                     JS_NewGlobalObject(rt.cx(), &SIMPLE_GLOBAL_CLASS, ptr::null_mut(),
                         OnNewGlobalHookOption::FireOnNewGlobalHook,
                         &*options)
                 });
 
+
+                rooted!(in(rt.cx()) let log = unsafe {
+                    JS_NewFunction(rt.cx(), Some(log), 0, 0, ptr::null_mut())
+                });
+
+                let console_class = JSClass {
+                    name: b"console\0" as *const u8 as *const i8,
+                    flags: 0,
+                    cOps: ptr::null(),
+                    spec: &ClassSpec {
+                        createConstructor: None,
+                        createPrototype: None,
+                        constructorFunctions: (),
+                        constructorProperties: (),
+                        prototypeFunctions: (),
+                        prototypeProperties: (),
+                        finishInit: None,
+                        flags: 0,
+                    },
+                    ext: ptr::null(),
+                    oOps: ptr::null(),
+                };
+
+                rooted!(in(rt.cx()) let console = unsafe {
+                    JS_NewGlobalObject(rt.cx(), &console_class, ptr::null_mut(),
+                        OnNewGlobalHookOption::FireOnNewGlobalHook,
+                        &*options)
+                });
+
+
                 rooted!(in(rt.cx()) let mut rval = UndefinedValue());
+
 
                 for _ in 0..reruns {
                     let _ = rt.evaluate_script(
@@ -65,4 +93,9 @@ impl JSRunner for SpiderMonkey {
             },
         )
     }
+}
+
+
+unsafe extern "C" fn log(cx: *mut JSContext, argc: ::std::os::raw::c_uint, vp: *mut Value) -> bool {
+
 }
